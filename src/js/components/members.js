0e4c4a0603257e00c12b0386636fbd16cdb8200e
developer-mustafa <mustafarahmanctg@gmail.com>
2025-11-25 00:11:05 +0600

    added modal in filter

--- src/js/components/members.js
+++ src/js/components/members.js
@@ -996,7 +996,6 @@ function _renderStudentCardsList() {
     .join('');
 
   elements.allStudentsCardsContainer.innerHTML = `
-    ${summary}
     <div class="grid items-stretch gap-6 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
       ${cards}
     </div>
